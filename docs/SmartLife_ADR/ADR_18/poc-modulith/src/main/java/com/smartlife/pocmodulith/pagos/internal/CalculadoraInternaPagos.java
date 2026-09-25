c9467d7f077e3c25b7bc07eb671bccb6620243d9
Anderson776-co <anderson.sanchez4591@uco.net.co>
2026-09-24 22:48:18 -0500

    Delete docs/SmartLife_ADR directory

--- docs/SmartLife_ADR/ADR_18/poc-modulith/src/main/java/com/smartlife/pocmodulith/pagos/internal/CalculadoraInternaPagos.java
+++
@@ -1,8 +0,0 @@
-package com.smartlife.pocmodulith.pagos.internal;
-
-public class CalculadoraInternaPagos {
-
-    public double calcularComision(double monto) {
-        return monto * 0.0265;
-    }
-}
